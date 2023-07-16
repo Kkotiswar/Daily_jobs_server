4612695785584ec4408e49f5b26158c8ed55fc79
Kotiswar <76087446+Kkotiswar@users.noreply.github.com>
2023-07-17 00:13:38 +0530

    port change

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ axios.get('https://github.com/bsovs/Fall2023-Internships')
     });
 
     // Start the server
-    const port = 3000 || process.env.PORT;
+    const port = process.env.PORT || 3000;
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
